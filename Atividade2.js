ede4cd81429cddcea8efec345f2c1eeb0ef57b9b
naamorim <101119934+naamorim@users.noreply.github.com>
2022-04-12 13:51:32 -0300

    Update Atividade2.js

---
+++ Atividade2.js
@@ -0,0 +1,23 @@
+<p> Cadastro de Eventos </p>
+("Informe a data do evento: ")
+if(data >= 27/03/2022){
+    console.log("Evento permitido");
+}
+else {
+    console.log("Não permitido, dados inválidos");
+}
+"Idade:"
+if(idade >= 18){
+    console.log("Cadastro permitido.");
+}
+else {
+    console.log("Não permitido pela idade.");
+}
+("Listar participantes e palestrantes: ")
+("Quantidade de participantes: ")
+if(quantidade <= 100){
+    console.log("Permitido. ");
+}
+else {
+    console.log("Não permitido por ter excedido o limite.");
+}
